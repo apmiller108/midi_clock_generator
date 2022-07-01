8f3bbd68501709ad66b5a736b2a00de58d5073c7
Alex Miller <apmiller@covermymeds.com>
2022-07-01 15:49:27 -0500

    Use spin locking for clock timer

--- MidiClockGenerator.cpp
+++ MidiClockGenerator.cpp
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <chrono>
 #include <thread>
+#include <math.h>
 #include "deps/RtMidi.h"
 
 // https://www.music.mcgill.ca/~gary/rtmidi/
@@ -18,21 +19,14 @@
 // g++ -Wall -D__MACOSX_CORE__ -o bin/mcg_macosx_core MidiClockGenerator.cpp RtMidi.cpp -framework CoreMIDI -framework CoreAudio -framework CoreFoundation -std=c++20 -pthread
 
 using namespace std;
-
-void sendMidiClock(RtMidiOut *o, int rate) {
-  // TODO: make this more accurate
-  while(true) {
-    std::vector<unsigned char> message{0xF8};
-    o->sendMessage(&message);
-    std::this_thread::sleep_for(std::chrono::microseconds(rate));
-  }
-}
+using namespace std::chrono;
 
 unsigned int selectBPM() {
   unsigned int bpm;
   cout << "Enter BPM" << endl;
   cin >> bpm;
   cout << "You entered: " << bpm << endl;
+
   return bpm;
 }
 
@@ -49,23 +43,74 @@ unsigned int selectOutputPort(RtMidiOut *midiOut, unsigned int nPorts){
     cout << "(" << i << ") : " << portName << endl;
   }
 
+  cout << "\nSelect MIDI output from the above options (enter the number)" << endl;
   cin >> portNumber;
-  cout << "\nSelect MIDI output." << endl;
+  cout << "You entered: " << portNumber << endl;
+
   return portNumber;
+}
+
+// This attempts to combine sleeping and spin-locking the CPU to generate an
+// accurate clock while using less CPU than full on spin locking.
+// Adapted from Blat Blatnik's blog post:
+// https://blat-blatnik.github.io/computerBear/making-accurate-sleep-function/
+void sendMidiClock(RtMidiOut *midiOut, double clockRateSecs) {
+  const vector<unsigned char> midiClockMessage{0xF8};
+
+  double estimate = 5e-3;
+  double mean = 5e-3;
+  double m2 = 0;
+  int64_t count = 1;
+
+  midiOut->sendMessage(&midiClockMessage);
+  auto prevTime = high_resolution_clock::now();
+  while (true) {
+    auto nextTime = high_resolution_clock::now();
+    double deltaTimeSecs = (nextTime - prevTime).count() / 1e9;
+    // printf("Message Time: %.2lf microseconds\n", deltaTimeSecs * 1e6);
+
+    auto messageTime = high_resolution_clock::now();
+    double waitSeconds = clockRateSecs - ((messageTime - nextTime).count() / 1e9);
+
+    // Sleep (0 CPU)
+    while(waitSeconds > estimate) {
+      auto startSleepTime = high_resolution_clock::now();
+      this_thread::sleep_for(milliseconds(1));
+      auto endSleepTime = high_resolution_clock::now();
+
+      double sleptForSeconds = (endSleepTime - startSleepTime).count() / 1e9;
+      waitSeconds -= sleptForSeconds;
+      // printf("Remaining wait: %.2lf mcs\n", waitSeconds * 1e6);
+
+      ++count;
+      double delta = sleptForSeconds - mean;
+      mean += delta / count;
+      m2 += delta * (sleptForSeconds - mean);
+      double stddev = sqrt(m2 / (count - 1));
+      estimate = mean + stddev;
+      // printf("Estimate: %.2lf mcs\n", estimate * 1e6);
+    }
+
+    // Spin lock (CPU thrash)
+    auto spinStart = high_resolution_clock::now();
+    while ((high_resolution_clock::now() - spinStart).count() / 1e9 < waitSeconds)
+    midiOut->sendMessage(&midiClockMessage);
+    prevTime = nextTime;
+  }
 }
 
 int main()
 {
   RtMidiOut *midiOut = new RtMidiOut();
 
-  const float microseconds = 60'000'000.0;
+  const float seconds = 60.0;
   const unsigned int ppq = 24;
 
   unsigned int bpm;
   bpm = selectBPM();
 
-  int clockRate = static_cast<int>(microseconds / (ppq * bpm));
-  cout << "MIDI Clock rate: " << clockRate << endl;
+  double clockRate = (seconds / (ppq * bpm));
+  cout << "MIDI Clock rate: " << (clockRate * 1e6) << " microseconds" << endl;
 
   // Check available MIDI output ports.
   unsigned int nPorts = midiOut->getPortCount();
